498ca416c6a45bf8246cf25fd99d324c0c50ba42
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-25 16:11:34 +0200

    dadi

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function OnlyMail(){
-  document.getElementById('Mail-Es').style.display='block';
+    document.getElementById('Mail-Es').style.display='block';
+    document.getElementById('Gioco-dadi').style.display='none';
 }
 function OnlyDice(){
-    
+    document.getElementById('Mail-Es').style.display='none';
+    document.getElementById('Gioco-dadi').style.display='block';
 }
 function Hint(){
     alert('prova con:(abcd,efg,1234,5678)@gmail.com');
@@ -26,4 +28,55 @@ function Verifica(){
         document.getElementById('hint').style.display='block';}
 
     document.getElementById('Email').value=null;
+}
+//gioco dei dadi
+function Cpu(){
+    const a = (Math.random() * (6-1) + 1).toFixed(0);
+    document.getElementById('begin').style.display='none';
+    document.getElementById('reset').style.display='block';
+    console.log(a);
+    if(a==1){
+        document.getElementById('one').style.display='block';
+    }
+    if(a==2){
+        document.getElementById('two').style.display='block';
+    }
+    if(a==3){
+        document.getElementById('three').style.display='block';
+    }
+    if(a==4){
+        document.getElementById('four').style.display='block';
+    }
+    if(a==5){
+        document.getElementById('five').style.display='block';
+    }
+    if(a==6){
+        document.getElementById('six').style.display='block';
+    }
+    const b = (Math.random() * (6-1) + 1).toFixed(0);
+    if(b==1){
+        document.getElementById('oneU').style.display='block';
+    }
+    if(b==2){
+        document.getElementById('twoU').style.display='block';
+    }
+    if(b==3){
+        document.getElementById('threeU').style.display='block';
+    }
+    if(b==4){
+        document.getElementById('fourU').style.display='block';
+    }
+    if(b==5){
+        document.getElementById('fiveU').style.display='block';
+    }
+    if(b==6){
+        document.getElementById('sixU').style.display='block';
+    }
+    if(a>b){
+        document.getElementById('Winner').innerHTML='hai Perso';
+        document.getElementById('Winner').style.color='red';
+    }else{
+        document.getElementById('Winner').innerHTML='hai Vinto';
+        document.getElementById('Winner').style.color='green';
+    }
 }
